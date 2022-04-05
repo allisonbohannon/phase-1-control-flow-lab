233e64476f24043176d6d83a2d3a46b32c369d91
Allison Bohannon <akboh.github@gmail.com>
2022-04-04 20:04:50 -0700

    commit tree 2cdcf718bbaab4381bbe93b62dcac4abc55cb636

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  switch (true) {
+    case distance <= 400: 
+      return "This one is on me!";
+      break;
+    case distance <= 2000: 
+      return 'That will be twenty bucks.'; 
+      break; 
+    case distance <= 2500: 
+      return 'I will gladly take your thirty bucks.'; 
+      break; 
+    case distance > 2500: 
+      return 'No can do.'; 
+      break;
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return (city == 'NYC'? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case "generous": 
+      return "Thank you so much."; 
+      break;
+    case "not as generous": 
+      return "Thank you."; 
+      break; 
+    default: 
+      return "Bye."
+  }
 }
